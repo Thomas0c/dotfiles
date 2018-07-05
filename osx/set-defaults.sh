9a9da4001967b6882b6830a4205519e0ff0f2954
Thomas Carlson <thomas@ustwo.com>
2018-07-05 20:04:03 +0200

    Adjust osx defaults and add editorconfig

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -7,6 +7,17 @@ defaults write com.apple.finder AppleShowAllFiles true
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Set Desktop as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
 # Set up Safari for development.
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
