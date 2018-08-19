90d910bcd14dc5090901285b2e6147b36578c7ef
Thomas Carlson <thomas.oca@gmail.com>
2018-08-19 13:41:19 +0200

    Add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,11 @@ set fish_greeting
 
 # Source fish files.
 source ~/.config/fish/gpg.fish
+
+# aliases
+alias gf="git fetch origin"
+alias fishconfig="code ~/.config/fish/config.fish"
+alias emulator="/Users/thomas/Library/Android/sdk/tools/emulator -avd Pixel_2_API_26"
+alias gs="git status"
+alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
+alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
